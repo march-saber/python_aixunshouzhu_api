071ebe494f73f51298a9bf71d962db1970f36acf
march-saber <1162869224@qq.com>
2019-06-01 17:56:05 +0800

    数据筛选

--- wifi_data/wifi_difference.py
+++ wifi_data/wifi_difference.py
@@ -10,7 +10,7 @@ class DoExcel:
 
     def get_case(self):
         max_row = self.sheet.max_row #最大行数
-        max_col = self.sheet.max_column  #最大列数
+        # max_col = self.sheet.max_column  #最大列数
         p = "(-)[1-9]*"
 
 
@@ -23,6 +23,7 @@ class DoExcel:
             try:
                 m = re.search(p,line_number)
                 g = m.group(0)
+
             except AttributeError as e:
                 print('第{0}行数据为空,日志{1}'.format(line,e))
             #去掉信号好强度：特点为负数
@@ -35,38 +36,35 @@ class DoExcel:
 
 if __name__ == '__main__':
     from openpyxl import load_workbook
-    do_excel1 = DoExcel('C:\\Users\\t\Desktop\数据记录\WiFi.xlsx','老版本第一次')
-    do_excel2 = DoExcel('C:\\Users\\t\Desktop\数据记录\WiFi.xlsx','Pro第一次')
+    from configparser import ConfigParser
+
+    cf = ConfigParser()
+    cf.read('wifi.conf', encoding='utf-8')
+    excel = cf.get('wifi_data', 'excel')
+    url = "C:\\Users\\t\Desktop\WiFi-data\\" + excel
+    print(url)
+
+    do_excel1 = DoExcel(url, 'old')
+    do_excel2 = DoExcel(url, 'new')
 
     cases1 = do_excel1.get_case()
     cases2 = do_excel2.get_case()
     # cases3 = []
     # cases4 = []
-    wb = load_workbook('C:\\Users\\t\Desktop\数据记录\WiFi.xlsx')
-    sheet3 = wb['交集']
-    sheet4 = wb['差集']
+    wb = load_workbook(url)
+    sheet4 = wb['difference']
     a = 0
-    for item in cases1:
-        if item in cases2:
-            # cases3.append(item)  #交集
-            a += 1
-            sheet3.cell(a,1).value = item
-            wb.save('C:\\Users\\t\Desktop\数据记录\WiFi.xlsx')
-    #差集
+
+    # 差集
     for item in cases1:
         if item not in cases2:
             # cases4.append(item)
             a += 1
             sheet4.cell(a, 1).value = item
-            wb.save('C:\\Users\\t\Desktop\数据记录\WiFi.xlsx')
+    wb.save(url)
     for item in cases2:
         if item not in cases1:
             # cases4.append(item)
             a += 1
             sheet4.cell(a, 1).value = item
-            wb.save('C:\\Users\\t\Desktop\数据记录\WiFi.xlsx')
-
-
-
-
-
+    wb.save(url)
